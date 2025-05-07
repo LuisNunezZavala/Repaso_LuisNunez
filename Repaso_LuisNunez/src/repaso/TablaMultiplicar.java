1b8d84dfb9efe7c9079dae4055eb77dfe4489910
Luis <Luis@DESKTOP-LUIS>
2025-05-07 12:03:21 +0200

    Ejemplo 1

---
+++ Repaso_LuisNunez/src/repaso/TablaMultiplicar.java
@@ -0,0 +1,12 @@
+package repaso;
+
+public class TablaMultiplicar {
+
+	public static void main(String[] args) {
+		
+		for (int i = 0; i <= 10; i++) {
+			System.out.println("10 * " + i + " = " + (10*i));
+			
+		}
+	}
+}
